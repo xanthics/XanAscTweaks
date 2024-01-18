db83be53dc05e6cdb64a58cbb3627a3f8c907f82
xan-asc <119889093+Xan-Asc@users.noreply.github.com>
2024-01-18 00:34:14 -0800

    Added bau in chat, dp, twitch support

--- XanAscTweaks/XanAscTweaks.lua
+++ XanAscTweaks/XanAscTweaks.lua
@@ -11,7 +11,16 @@ function XAT:setColor(val)
     return "|cFFFFBF00" .. val .. "|r"
 end
 
-function XAT:printmsg(message)
+local function status(val)
+    if val then
+        return "|cFF00FF00On|r"
+    end
+    return "|cFFFF0000Off|r"
+
+end
+
+function XAT:printmsg(message, ...)
+
     DEFAULT_CHAT_FRAME:AddMessage(XAT:setColor("XAT") .. ": " .. message)
 end
 
@@ -19,7 +28,7 @@ end
 local function toggle(var, text)
     if var then
         XAT:printmsg("`"..text.."` is deactivated.")
-        return nil
+        return false
     else
         XAT:printmsg("`"..text.."` is now active.")
         return true
@@ -53,29 +62,34 @@ function XAT:CommandHandler(msg)
         reload = true
 	elseif cmd == "coa" then
 		XanAscTweaks.filterCOA = toggle(XanAscTweaks.filterCOA, "Conquest of Azeroth Travel Guide")
-        reload = true
 	elseif cmd == "bau" then
 		XanAscTweaks.filterBAU = toggle(XanAscTweaks.filterBAU, "Northrend Travel Guide")
-        reload = true
-	else
-		XAT:printmsg("Use '/xat option` where option can be one of; say, yell, button, trial, altar, autobroadcast, new, ascension, world, coa, bau.")
+	elseif cmd == "bauchat" then
+		XanAscTweaks.filterBAUAsc = toggle(XanAscTweaks.filterBAUAsc, "bau in chat")
+	elseif cmd == "dp" then
+		XanAscTweaks.filterDP = toggle(XanAscTweaks.filterDP, "dp in chat")
+	elseif cmd == "twitch" then
+		XanAscTweaks.filterTwitch = toggle(XanAscTweaks.filterTwitch, "Twitch in chat")
+ 	else
+		XAT:printmsg("Use '/xat option` where option can be one of; say, yell, button, trial, altar, autobroadcast, new, ascension, world, coa, bau, bauchat, dp, twitch.", true)
         local options = {
-            XanAscTweaks.filtersay and "Say is being filtered" or nil,
-            XanAscTweaks.filteryell and "Yell is being filtered" or nil,
-            XanAscTweaks.hideAscButton and "Ascension Button is being hidden" or nil,
-            XanAscTweaks.filtertrial and "Trials Broadcasts are being filtered" or nil,
-            XanAscTweaks.filterMEA and "Mystic Enchanting Altar Broadcasts are being filtered" or nil,
-            XanAscTweaks.filterAuto and "Autobroadcasts are being filtered" or nil,
-            XanAscTweaks.filterNew and "Newcomers is being removed from first chat window" or nil,
-            XanAscTweaks.filterAscension and "Ascension is being removed from first chat window" or nil,
-            XanAscTweaks.filterWorld and "World is being removed from first chat window" or nil,
-            XanAscTweaks.filterCOA and "Conquest of Azeroth Travel Guide is being filtered" or nil,
-            XanAscTweaks.filterBAU and "Northrend Travel Guide is being filtered" or nil,
+            status(XanAscTweaks.filtersay).." `say` removed in rest areas",
+            status(XanAscTweaks.filteryell).." `yell` removed in rest areas",
+            status(XanAscTweaks.hideAscButton).." `button` is hiding Ascension Button",
+            status(XanAscTweaks.filtertrial).." `trial` Broadcasts are being filtered",
+            status(XanAscTweaks.filterMEA).." `altar` is hiding Mystic Enchanting Altar Broadcasts",
+            status(XanAscTweaks.filterAuto).." `autobroadcast` messages are being hidden",
+            status(XanAscTweaks.filterNew).." `new` is removing Newcomers from first chat tab",
+            status(XanAscTweaks.filterAscension).." `ascension` is removing Ascension from first chat tab",
+            status(XanAscTweaks.filterWorld).." `world`  is removing World from first chat tab",
+            status(XanAscTweaks.filterCOA).." `coa` is filtering Conquest of Azeroth Travel Guide",
+            status(XanAscTweaks.filterBAU).." `bau` is filtering Northrend Travel Guide",
+            status(XanAscTweaks.filterBAUAsc).." `bauchat` is hiding BAU from Ascension and Newcomers",
+            status(XanAscTweaks.filterDP).." `dp` is hiding messages that contain dp and don't contain dps",
+            status(XanAscTweaks.filterTwitch).." `twitch` is hiding twitch links in Ascension and Newcomers",
         }
         for _,option in pairs(options) do
-            if option then
-                XAT:printmsg(option)
-            end
+            XAT:printmsg(option, false)
         end
 	end
     if reload then
@@ -97,7 +111,7 @@ function XAT:hideNew()
 end
 
 -- hide say/yell when in a city
-local function filterAll(self, event, ...)
+local function filterAll(self, event, ...)    
     if IsResting() then
         if event == "CHAT_MSG_SAY" and XanAscTweaks.filtersay then
             return true
@@ -110,7 +124,7 @@ local function filterAll(self, event, ...)
 end
 
 -- filter system messages to remove various unwanted messages
-local function filtersystem(self, event, msg, ...)
+local function filterSystem(self, event, msg, ...)
 	if event ~= "CHAT_MSG_SYSTEM" or not msg then return false end
 
     for filter,_ in pairs(filters) do
@@ -123,6 +137,26 @@ local function filtersystem(self, event, msg, ...)
 	return false
 end
 
+-- remove BAU and DP from newcomers and ascension
+local function filterChannel(self, event, msg, ...)
+    local channel = select(8, ...)
+    channel = channel:lower()
+    if event ~= "CHAT_MSG_CHANNEL" or not msg or (channel ~= "ascension" and channel ~= "newcomers") then return false end
+
+    local msglower = msg:lower()
+
+    if XanAscTweaks.filterBAUAsc and msglower:find("bau") then
+        return true
+    end
+    if XanAscTweaks.filterDP and not msglower:find("dps") and msglower:find("dp") then
+        return true
+    end
+    if XanAscTweaks.filterTwitch and msglower:find("twitch") then
+        return true
+    end
+    return false
+end
+
 -- At character login set up a command handler and our variables
 function XAT:XanEventHandler(event, ...)
 	self[event](self, event, ...)
@@ -133,19 +167,11 @@ function XAT.frame:ADDON_LOADED(event, ...)
 	self:UnregisterEvent("ADDON_LOADED")
 
     if XanAscTweaks == nil then
-		XanAscTweaks = {
-            filtersay = true,
-            filteryell = true,
-            hideAscButton = true,
-            filtertrial = true,
-            filterMEA = true,
-            filterAuto = true,
-            filterNew = true,
-            filterAscension = true,
-            filterWorld = true,
-            filterCOA = true,
-            filterBAU = true,
-        }
+		XanAscTweaks = {}
+        for _,v in pairs({"filtersay", "filteryell", "hideAscButton", "filtertrial", "filterMEA", "filterAuto", "filterNew", 
+                          "filterAscension", "filterWorld", "filterCOA", "filterBAU", "filterBAUAsc", "filterDP", "filterTwitch"}) do
+            XanAscTweaks[v] = true
+        end
 	end
 end
 
@@ -161,9 +187,10 @@ function XAT.frame:PLAYER_ENTERING_WORLD(event, ...)
         LibDBIcon10_AscensionUICA2:Hide()
     end
 
-    ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", filtersystem)
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", filterSystem)
     ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filterAll)
     ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filterAll)
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filterChannel)
 
     XAT:wait(1, XAT.hideNew, self)
 
